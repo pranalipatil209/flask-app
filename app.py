8c600cacfcb1fec3370851eb4d7bf0dcef015e42
Pranali <pranalipatil209@gmail.com>
2019-09-23 18:49:34 +0530

    python text app

---
+++ app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+import process
+do_work = process.FILE_HANDELING()
+@app.route('/',defaults={'filename': 'file1.txt'})
+@app.route('/<filename>', methods=['GET'])
+def readFile(filename):
+    start = request.args.get('startline', default=0, type=int)
+    end = request.args.get('endline', default=0, type=int)
+    data = do_work._valid_file(filename)
+    if data is True:
+        temp = do_work._read_file(filename,start,end)
+        if(temp == True):
+            return render_template('output.html')
+        else:
+            return render_template('404.html')
+    else:
+        return render_template('404.html')
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ process.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import json
+import re
+import codecs
+
+class FILE_HANDELING(object):
+    def __init__(self):
+        self.files = ["file1", "file2", "file3", "file4"]
+
+    def _read_file(self, filename, start, end):
+        try:
+            startline = 0
+            endline = 0
+            f = codecs.open(r"datasource/"+filename,"r+", encoding="utf-8", errors='ignore')
+            f1 = open(r'templates/output.html', 'w')
+            f1.write(r'{% extends "index.html" %}')
+            f1.write(r'{% block content %}')
+            total_lines = f.readlines()
+            length = len(total_lines)
+            if(start >= 0 and start < length and end >= 0 and  end < length and end > start):
+                startline = start
+                endline = end - 1
+            else:
+                endline = length - 1
+            for i, x in enumerate(total_lines):
+                if i >= startline:
+                    f1.write(x)
+                    f1.write("<br>")
+                    if i > endline:
+                        break
+            f1.write(r'{% endblock %}')
+            f.close()
+            f1.close()
+            return True
+        except Exception, ex:
+            pass
+
+    def _valid_file(self, name):
+        file_name = re.split(r'\s',name)
+        valid = False
+        for file in self.files:
+            if(re.search(file,file_name[0])):
+                valid = True
+        return valid
